de8f0b03373e1d1a0001caa4e58dd43028e28e9d
pedrocarmona <pcarmona1990@gmail.com>
2014-04-11 15:12:08 +0100

    local changes

--- MouseBits_sv/src/main/java/Server.java
+++ MouseBits_sv/src/main/java/Server.java
@@ -1,3 +1,5 @@
+import robot.MouseMove;
+
 /**
  * Created by joao on 4/11/14.
  */
@@ -6,6 +8,7 @@ public class Server {
     public static void main(String[] args) {
         Thread waitThread = new Thread(new WaitThread());
         waitThread.start();
+        //MouseMove mouseMove = new MouseMove();
     }
 
 }
